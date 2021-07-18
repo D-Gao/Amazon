9e192c2660a072595bffeadf98ee21f579feebfd
D-Gao <gaoheng.huang@mail.polimi.it>
2021-07-18 18:17:52 +0200

    webpage header created

--- amazon_clone/src/App.js
+++ amazon_clone/src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello programmer</h1>
-
+    <div className="app">
+   
+     <Header/>
+      
     </div>
   );
 }
