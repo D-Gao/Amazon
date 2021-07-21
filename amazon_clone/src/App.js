df26545b5e6d55196c9fce04d632fa61ab445ec7
D-Gao <gaoheng.huang@mail.polimi.it>
2021-07-21 15:55:14 +0200

    finished checkout&redux part

--- amazon_clone/src/App.js
+++ amazon_clone/src/App.js
@@ -1,14 +1,30 @@
 //import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
-import Home from './Home'; 
+import Home from './Home';
+import Checkout from './Checkout';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";;
 
 function App() {
   return (
     <div className="app">
-   
-     <Header/>
-     <Home/>
+      <Router>
+      <Header />
+        <Switch>
+
+        <Route path="/checkout">
+        <Checkout />
+           
+         </Route>
+
+          <Route path="/">
+         
+          <Home />
+          </Route>
+         
+        </Switch>
+      </Router>
+
     </div>
   );
 }
--- amazon_clone/src/Header.jsx
+++ amazon_clone/src/Header.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import "./Header.css"
+import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search"
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
+import { useStateValue } from "./StateProvider";
+
+
 function Header() {
+    const [{ basket, user }, dispatch] = useStateValue();
+
     return (
         <div className="header">
+            <Link to="/">
             <img 
                 className="header_logo"
                 src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
             />
+
+            </Link>
+           
             <div className="header_search">
                 <input
                     className="header_searchInput"
@@ -37,11 +47,16 @@ function Header() {
                 </div>
 
 
-                <div className="header_optionBasket">
-                    <ShoppingBasketIcon className=""/>
-                    <span className="header_optionLineTwo header_basketCount" 
-                    >0</span>
-                </div>
+
+
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header__optionLineTwo header__basketCount">
+                            {basket?.length}
+                        </span>
+                    </div>
+                </Link>
             </div>
         </div>
     )
